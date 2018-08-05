93e763f62bd1bcb12c963b06b2ab3bad9ac87813
DuyTran317 <tdd310797@gmail.com>
2018-08-05 17:47:50 +0700

    Add_files v.5

--- ProjectOrder/module/back/home_thanhtoan.php
+++ ProjectOrder/module/back/home_thanhtoan.php
@@ -1,10 +1,82 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<?php
+	if(! isset($_SESSION['admin_id']))
+	{
+		header("location:?mod=dangnhap");	
+	}
+	
+?>
+<!--Reload Page-->
+<meta http-equiv="refresh" content="number;url=http://localhost/project-orderfood/ProjectOrder/admin.php?mod=home_thanhtoan">
 
-<body>
-</body>
-</html>
+<style>
+ table.dataTable{
+	 border-collapse:collapse;
+ }
+</style>
+
+<div class="container-fluid"> 
+ <div class="row">
+  <div style="padding-bottom:25px; padding-top:25px" class="col-md-12 col-sm-12 col-xs-12">  
+  <caption>
+  	<p style="text-align:center"><a href="?mod=home_thanhtoan"><button class="btn btn-success">Làm Mới</button></a></p>
+    <h2 style="color:#096; text-align:center"><i class="fas fa-clipboard-list"></i> Danh Sách Chờ Thanh Toán</h2>
+  </caption>
+  </div>
+ </div> 
+</div>  
+  
+<table class="col-md-12 col-sm-12 col-xs-12 table-bordered" id="datatable" style="text-align:center; margin-top:15px">
+  <thead>
+  <tr align="center" bgcolor="#FFFFCC">
+    <td><h4><strong>STT</strong></h4></td>
+    <td><h4><strong>Số Bàn</strong></h4></td>
+    <td><h4><strong>Trạng Thái</strong></h4></td>    
+    <td><h4><strong>Kiểm Tra</strong></h4></td>
+  </tr>
+  </thead>
+  <?php   	
+	$sql = "SELECT *, b.`active` as wait from `of_order` as a, `of_bill` as b where a.`id`=b.`order_id` and a.`active`=1 and b.`active`=0";
+	$rel = mysqli_query($link,$sql);
+	$i=1;
+	while($re = mysqli_fetch_assoc($rel))
+	{
+  ?>
+  <tr>
+  	<td align="center"><h5>
+      <?=$i++?>
+    </h5></td>
+    <td align="center"><h5>&nbsp;&nbsp;
+      Bàn Số <?=$re['num_table']?>
+    </h5></td>
+    <td align="center"><h5>
+      <?php
+	  	if($re['wait']==0)
+		echo"Đang chờ thanh toán";
+	  ?>
+    </h5></td>   
+    <td align="center"><h5>
+      <a href="?mod=payment&id=<?=$re['order_id']?>&num_table=<?=$re['num_table']?>">Xem</a>
+    </h5></td> 
+  </tr>
+  <?php } ?>
+</table>
+
+<script>
+	$(document).ready(function(){    	
+		$('#datatable').DataTable( {
+   			 language: {
+        		url: 'datatable/Vietnamese.json'
+    		}
+		});
+    });
+	
+	//Reload Page
+	init_reload();
+	var time= getElementByID('#time_refresh');
+    function init_reload(){
+        setInterval( function() {
+                   window.location.reload();
+ 
+          },10000);
+    }
+</script>
--- ProjectOrder/module/back/solve_payment.php
+++ ProjectOrder/module/back/solve_payment.php
@@ -1,10 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<?php
+	if(! isset($_SESSION['admin_id']))
+	{
+		header("location:?mod=dangnhap");	
+	}
+	if(isset($_GET['orderID']))
+	{
+		$orderID=$_GET['orderID'];
+	}
+	if(isset($_GET['num_table']))
+	{
+		$num_table=$_GET['num_table'];
+	}
+	if(isset($_GET['total']))
+	{
+		$total=$_GET['total'];
+	}	
+	
+	$sql="update `of_bill` set `active`=1 where `order_id`={$orderID}";
+	mysqli_query($link,$sql);
 
-<body>
-</body>
-</html>
+?>
+<script>
+	alert("Hoàn Tất Thanh Toán!");
+	window.location="?mod=home_thanhtoan";
+</script>
