4e0e5b9732625a77783199e3be71c240da4afee6
Duy Tran <tdd310797@gmail.com>
2018-08-31 14:47:30 +0700

    FixTempLayOut Home

--- ProjectOrder/module/front/home.php
+++ ProjectOrder/module/front/home.php
@@ -13,51 +13,42 @@ if(isset($_GET['name']))
 }
 ?>
 
-<style>
-.responsive {
-    max-width: 100%;
-	height:auto;
-}
-</style>
-<body style="background-color: #fff289; font-family: 'Pacifico', cursive;">
-<img src="img/front/black-pepper-bright-colors-940302.png"  class="responsive" width="100%" />
-<div class="container" style="margin-top: -8%; margin-bottom: 5%" >
-    <div class="row" >
-    <?php
-		$sql="select * from `of_category` where `active`=1";
-		$rs=mysqli_query($link,$sql);
-		while($r=mysqli_fetch_assoc($rs)):
-	?>
-    <a href="?mod=menu&id=<?=$id?>&name=<?=$name?>&cate=<?=$r['id']?><?php if(isset($_GET['thanhtoan'])){echo "&thanhtoan=1";}?>">
-        <div class="col-sm-4">
-            <div class="col-xs-12" style="background-color:#FFF; ">
-                <div class="row">
-                    <div class="col-xs-12" style="background-image: url(img/front/<?php 
-					if($r['name']=='Đồ Ăn')
-					{
-						echo "close-up-cooking-cuisine-958545.jpg";
-					}
-					elseif($r['name']=='Thức Uống')
-					{
-						echo "blur-close-up-cutlery-370984.jpg";
-					}
-					else
-					{
-						echo "appetizer-breakfast-cuisine-326278.jpg";
-					}
-					?>); height: 200px; background-position: center; background-size: contain;"> </div>
-                    <div class="col-xs-12" style=" padding: 20px;">
-                        <p align="center" style="font-size: 25px; border-bottom: black; color: grey; font-weight: bold"><?=$r['name']?></p>
+<body style="background-color: #fff289; font-family: 'Pacifico', cursive; background-image:url(img/front/1584.jpg); background-position:center; background-size:cover; background-attachment:fixed">
 
-                    </div>
-                </div>
+<h1 style="color:#FFF; text-decoration:underline">Bàn <?=$name?></h1>
 
-            </div>
-        </div>
-     </a>   
-     <?php endwhile ?>         
-    </div>
+<div style="text-align:center; font-size:38px; margin-top:80px; color:#900">
+	<p>Nhà Hàng 5 Con Dê Xin Hân Hạnh Phục Vụ Quý Khách !</p>
 </div>
-<img src="img/front/Untitled-1.png"  class="responsive" width="100%" />
+
+<div class="container-fluid" style="margin-top:50px">
+<div class="row">
+    
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <?php
+            $sql="select * from `of_category` where `active`=1";
+            $rs=mysqli_query($link,$sql);
+			$dem=0;
+            while($r=mysqli_fetch_assoc($rs)):
+			$dem++;
+        ?>
+            <div style="text-align:center; border:1px solid #F60; margin-top:40px" class="col-md-4 col-sm-6 col-xs-12 btn btn-warning">
+            	<a href="?mod=menu&id=<?=$id?>&name=<?=$name?>&cate=<?=$r['id']?><?php if(isset($_GET['thanhtoan'])){echo "&thanhtoan=1";}?>" style="font-size:40px ;text-decoration:none;">
+    
+                   <div class="<?php if($dem%2==0) echo 'noibat2'; else echo 'noibat';?>" style="padding:70px 0 70px 0; background-color:#FF9"><i class="fas fa-utensils"></i> <?=$r['name']?></div>
+      
+                </a>
+            </div>         
+            
+         <?php endwhile ?>         
+    </div>
+
+</div>
+</div>
+
+<div style="text-align:center; font-size:38px; margin-top:70px; color:#F00">
+	<marquee scrollamount="12">*** Chúc Quý Khách Ngon Miệng ***</marquee>
+</div>
+
 </body>
 </html>
