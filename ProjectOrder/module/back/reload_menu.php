4fcfed6a495d56ccc00d8cd222343be1b04347a1
Genos <nhatnam2405@gmail.com>
2018-09-21 11:26:19 +0700

    riu tham load menu.php

---
+++ ProjectOrder/module/back/reload_menu.php
@@ -0,0 +1,19 @@
+<?php
+	require('Pusher.php');
+	$options = array(
+	'cluster' => 'ap1',
+    'encrypted' => true
+	);
+ 	$pusher = new Pusher(
+    '161363aaa8197830a033',
+    '46f2ba3b258f514f6fc7',
+    '577033',
+    $options
+	);
+	$data['name'] = 'Load Menu';
+	$data['message'] = 'Cap Nhat ';
+	$pusher->trigger('hihi', 'loadmenu', $data);
+?>
+<script>
+	window.location="?mod=ds_food";
+</script>
