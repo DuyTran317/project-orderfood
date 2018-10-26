a85fbef63f7228fd0ca7919e769fc80f06fe09a9
Duy Tran <tdd310797@gmail.com>
2018-10-26 17:06:04 +0700

    Del_session

--- ProjectOrder/module/front/xulydangxuat.php
+++ ProjectOrder/module/front/xulydangxuat.php
@@ -6,5 +6,6 @@ $rs=mysqli_query($link,$sql);
 unset($_SESSION['user_idban']);
 unset($_SESSION['user_nameban']);
 unset($_SESSION['cart']);
+unset($_SESSION['order_wait']);
 header("location:?mod=dangnhap");
 ?>
