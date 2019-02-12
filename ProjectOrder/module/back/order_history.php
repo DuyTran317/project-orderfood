2b8420e997171863586778d6388690bcf3c58e1d
Duy Tran <tdd310797@gmail.com>
2019-02-12 15:45:10 +0700

    New

--- ProjectOrder/module/back/order_history.php
+++ ProjectOrder/module/back/order_history.php
@@ -1,11 +1,50 @@
-
+<script>
+	$( function() {
+	$( "#datepicker" ).datepicker({
+	  showOn: "button",
+	  buttonImage: "jquery/jquery-ui-1.12.1.custom/images/calendar.gif",
+	  buttonImageOnly: true,
+	  buttonText: "Select date",
+	  
+	  //Tùy chỉnh tháng
+	  changeMonth:true,
+	  monthNamesShort: [ "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12" ],
+	  
+	  //Tùy chỉnh năm
+	  changeYear:true,
+	  yearRange: "c-100:c",
+	  
+	  //Date Format
+	  dateFormat: "dd-mm-yy",
+	  
+	});
+	
+	} );
+</script>
 <script src="lib/notice.js"></script>
-<?php
-if(! isset($_SESSION['admin_id']))
-{
-    header("location:?mod=dangnhap");
-}
 
+<?php
+	if(! isset($_SESSION['admin_id']))
+	{
+		header("location:?mod=dangnhap");
+	}
+	
+	//Khởi tạo
+	$datefrom = date("Y-m-d 00:00:00");
+	$dateto = date("Y-m-d 23:59:59");	
+	
+	if(isset($_POST['date_pick']))
+	{
+		$date_pick=$_POST['date_pick'];
+		
+		//Chuyen format $date_pick tu dd/mm/yyyy -> yyyy-mm-dd
+		$d= substr($date_pick,0,2);
+		$m= substr($date_pick,3,2);
+		$y= substr($date_pick,6,4);
+		
+		$date_pickfrom="{$y}-{$m}-{$d} 00:00:00";
+		$date_pickto="{$y}-{$m}-{$d} 23:59:59";		
+	}
 ?>
 <!--Reload Page-->
 <!--<meta http-equiv="refresh" content="number;url=http://localhost/project-orderfood/ProjectOrder/admin.php?mod=home">
@@ -38,29 +77,55 @@ if(! isset($_SESSION['admin_id']))
             <h2 style=" text-align:center;">Lịch Sử Đặt Món</h2>
         </div>
         <div class="table-responsive"></div>
+        
+        <ul>
+            	<h4>Tìm theo ngày</h4>
+                <form action="" method="post">
+                <input type="text" name="date_pick" id="datepicker" readonly
+                value="<?php if(isset($date_pick)) echo $date_pick;?>" >
+                <button type="submit">Chọn</button>
+                </form>	
+        </ul>
+        
         <table class="col-md-12 col-sm-12 col-xs-12 table-bordered" id="datatable" style=" margin-top:15px; overflow-x: scroll">
             <thead>
             <tr style="font-size:20px">
                 <td align="center">Số Bàn</td>
-                <td>Giờ Đặt</td>
+                <td align="center">Giờ Đặt</td>
                 <td align="center">Trạng Thái</td>
                 <td align="center"></td>
             </tr>
             </thead>
+            	<?php
+					if(isset($_POST['date_pick']))
+					{
+					$sql="select * from `of_order` where `date` >='{$date_pickfrom}' and `date` <='{$date_pickto}' order by `date` desc";
+					}
+					if(!isset($_POST['date_pick']))
+					{
+					$sql="select * from `of_order` where `date` >='{$datefrom}' and `date` <='{$dateto}' order by `date` desc";	
+					}
+					$rs = mysqli_query($link,$sql);
+					while($r=mysqli_fetch_assoc($rs)){
+				?>
                 <tr>
                     <td align="center" style="color:#906">
-                       Bàn 1
+                       Bàn <?=$r['num_table']?>
                     </td>
-                    <td align="left">&nbsp;&nbsp;
-                        <span style=" font-size: 18px;">15/5/2019 69:69:96</span>
+                    <td align="center">&nbsp;&nbsp;
+                        <span style=" font-size: 18px;"><?=date('H:i',strtotime($r['date']))?></span>
                     </td>
                     <td align="center">
-                        Đang Mần
+                        <?php
+							if($r['active']==2)echo "Đang Chờ";
+							if($r['active']==1)echo "Đã Hoàn Thành";
+						?>
                     </td>
                     <td align="center">
                         <a data-toggle="modal" data-target="#table_info" >Chi Tiết</a>
                     </td>
                 </tr>
+                <?php } ?>
         </table>
     </div>
 
