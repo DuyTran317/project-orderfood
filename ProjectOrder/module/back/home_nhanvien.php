06f804063b845fa24d168f9d3c61b07ec08b9a65
Khoa <dodangkhoagtvt@gmail.com>
2019-02-15 14:08:43 +0700

    commit cc

--- ProjectOrder/module/back/home_nhanvien.php
+++ ProjectOrder/module/back/home_nhanvien.php
@@ -121,7 +121,7 @@
 		$sql1="SELECT * FROM `of_user` ORDER BY (CASE `active` WHEN '2' THEN 1 END) DESC , `name` ASC";
 	*/
 
-	$sql1="SELECT *, a.`active` as a_user FROM `of_user` as a left JOIN `of_order` as b ON a.`name` = b.`num_table` order by b.`date` desc, a.`name` asc"; 
+	$sql1="SELECT *, a.`active` as a_user FROM `of_user` as a left JOIN `of_order` as b ON a.`name` = b.`num_table` order by a.`active` desc, b.`active` asc, a.`name` asc"; 
 	$c=mysqli_query($link,$sql1);
 	
 	while($slban=mysqli_fetch_assoc($c)):
