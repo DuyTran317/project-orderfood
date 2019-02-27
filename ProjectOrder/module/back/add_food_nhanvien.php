953ffb77d92b143642eea07418788304150de844
Genos <42258883+nhatnam2405@users.noreply.github.com>
2019-02-27 13:34:14 +0700

    riu tham nhan vien dat mon cho bep

--- ProjectOrder/module/back/add_food_nhanvien.php
+++ ProjectOrder/module/back/add_food_nhanvien.php
@@ -57,8 +57,20 @@ if(empty($_SESSION['servantlang'])){
 				//Insert note vao DB
 				$sql="insert into `of_note_order` values(NULL,'$id','$note',2)";
 				mysqli_query($link,$sql);
-		
-				
+				//riu tham trang bep
+				require('Pusher.php');
+				$options = array(
+				'cluster' => 'ap1',
+				'encrypted' => true
+				);
+				$pusher = new Pusher(
+				'161363aaa8197830a033',
+				'46f2ba3b258f514f6fc7',
+				'577033',
+				$options
+				);
+				$pusher->trigger('Reload', 'reloadbep', @$data);
+					
 				echo "
 				<script>
 					alert('Đã thêm thành công!');
