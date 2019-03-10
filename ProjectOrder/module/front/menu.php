a051225cea56320ed32db9db09e7c844db74bb02
Duy Tran <tdd310797@gmail.com>
2019-03-10 09:52:57 +0700

    Fix Bugs

--- ProjectOrder/module/front/menu.php
+++ ProjectOrder/module/front/menu.php
@@ -70,12 +70,10 @@ Pusher.logToConsole = true;
             <a style="font-size: 25px; text-decoration: none; color: black; padding:5px; background-color: orange; font-style: italic; font-weight: bold; position: absolute; top: 10px; right: 0px;"  href="index.php?mod=cart_process&id_ban=<?=$id?>&name_ban=<?=$name?>&act=4<?php if(isset($_GET['thanhtoan'])) echo '&thanhtoan=1'?>">skip >></a>
             <script>
                 <?php
-                $catename = "select * from `of_department` where `id`={$cate}";
-                $get = mysqli_query($link, $catename);
-                $result = mysqli_fetch_assoc($get);
+				$result = selectIdWithCondition($link, 'of_department', $cate);
                 ?>
-                var duyoccho = confirm("Bạn có muốn chọn <?=$result['vi_name']?> không?");
-                if (duyoccho == false) {
+                var check = confirm("Bạn có muốn chọn <?=$result['vi_name']?> không?");
+                if (check == false) {
                     window.location.href = "index.php?mod=cart_process&id_ban=<?=$id?>&name_ban=<?=$name?>&act=4<?php if (isset($_GET['thanhtoan'])) echo '&thanhtoan=1'?>";
                 }
                 <?php ?>
