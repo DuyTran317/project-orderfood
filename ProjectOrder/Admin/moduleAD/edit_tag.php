318cb6fbd5d3ae57b3de6b1f8447dc93015c8a4f
kietnguyen161 <kietnguyen161>
2019-04-26 10:48:03 +0700

    add edit tag, delete tag in admin

--- ProjectOrder/Admin/moduleAD/edit_tag.php
+++ ProjectOrder/Admin/moduleAD/edit_tag.php
@@ -4,16 +4,25 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                <?=_ADD?>
+                <?=_EDIT?>
                 <small>Tag</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="trang-chu.html"><i class="fa fa-dashboard"></i><?=_HOME?></a></li>
                 <li><a href="danh-sach-tag.html">Tag</a></li>
-                <li class="active"><?=_ADD?></li>
+                <li class="active"><?=_EDIT?></li>
             </ol>
         </section>
+        <?php
+        if(isset($_GET['edit'])) {
+            $edit = $_GET['edit'];
 
+        }
+        $sql_edit = "select * from of_tag where id=$edit";
+        $kq_edit = mysqli_query($link,$sql_edit);
+        $d_edit=mysqli_fetch_assoc($kq_edit);
+         
+        ?>
         <!-- Main content -->
         <section class="content">
             <div class="row">
@@ -23,24 +32,25 @@
                     <div class="box box-primary">
                         <!-- form start -->
                         <form role="form" method="post" enctype="multipart/form-data" action="process-tag.html">
+                            <input type="hidden" value="<?= $d_edit['id']?>" name="id">
                             <div class="box-body">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1"><?=_NAME?> VN: <span style="color:#F00" >(*)</span></label>
                                     <input type="text" class="form-control"
-                                           required  name="vi_tag" placeholder="<?=_NAME?> (VN)">
+                                           required  name="edit_vi_tag" value = "<?= $d_edit['vi_name'] ?>">
                                 </div>
                             </div>
                             <div class="box-body">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1"><?=_NAME?> EN: <span style="color:#F00" >(*)</span></label>
                                     <input type="text" class="form-control"
-                                           required  name="en_tag" placeholder="<?=_NAME?> (EN)">
+                                           required  name="edit_en_tag" value = "<?= $d_edit['en_name'] ?>">
                                 </div>
                             </div>
                             <!-- /.box-body -->
 
                             <div class="box-footer">
-                                <button type="submit" name="add_tag" class="btn btn-primary"><?=_ADD?></button>
+                                <button type="submit" name="edit_tag" class="btn btn-primary"><?=_EDIT?></button>
                                 <button type="reset" class="btn btn-defaul"><?=_RESET?></button>
                             </div>
                         </form>
--- ProjectOrder/Admin/moduleAD/process_tag.php
+++ ProjectOrder/Admin/moduleAD/process_tag.php
@@ -5,7 +5,7 @@
         if (!addTag($link, $_POST['vi_tag'], $_POST['en_tag'])) {
             echo "<script> alert('thêm tag thất bại!'); </script>";
         }
-        echo "<script> window.location = '?mod=add_tag';</script>";
+        echo "<script> window.location = 'danh-sach-tag.html';</script>";
     }
     if(isset($_POST['save_tags_food']))
     {
@@ -14,5 +14,25 @@
             saveTagsFood($link,$_POST['food_id']);
         }
         echo "<script> window.location = '?mod=add_food_tag';</script>";
+    }
+
+
+
+    if(isset($_POST['edit_tag']))
+    {
+        if (!editTag($link, $_POST['edit_vi_tag'], $_POST['edit_en_tag'])) {
+            echo "<script> alert('thêm tag thất bại!'); </script>";
+        }
+        echo "<script> window.location = 'danh-sach-tag.html';</script>";
+    }
+    
+
+    if(isset($_GET['del']))
+    {
+        $sql_del = sql_delete_de('of_tag');;
+        if(mysqli_query($link,$sql_del))
+        {
+            header("location:danh-sach-tag.html");
+        }else echo $sql_del;
     }
 ?>
